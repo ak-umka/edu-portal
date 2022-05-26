4271c3b6f2fe21f861506d1aa398bd3ae0c96ec6
ak-umka <umbet70@gmail.com>
2022-05-26 17:56:46 +0600

    remove func from app

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 
-const jwt = require('jsonwebtoken');
+
 
 const errorMiddleware = require('./middlewares/error-middleware');
 const router = require('./routes/auth-route');
 const postRouter = require('./routes/post-route');
 const subdRouter = require('./routes/subd-route');
 
-const User = require('./models/user-model');
+
 
 dotenv.config();
 
@@ -28,22 +28,6 @@ app.use('/api/v0/', router, postRouter, subdRouter);
 app.use(errorMiddleware);
 
 
-app.use(async (req, res, next) => {
-    if (req.headers["x-access-token"]) {
-        const accessToken = req.headers["x-access-token"];
-        const { userId, exp } = await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-        // Check if token has expired
-        if (exp < Date.now().valueOf() / 1000) {
-            return res.status(401).json({
-                error: "JWT token has expired, please login to obtain a new one"
-            });
-        }
-        res.locals.loggedInUser = await User.findById(userId);
-        next();
-    } else {
-        next();
-    }
-});
 
 const start = async () => {
     try {
